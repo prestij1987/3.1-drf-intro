a660edb06b63362e7a8ac61db8343f61eacb01ba
prestij1987 <lehaleyba@mail.ru>
2022-06-02 07:11:35 +0300

    Initial commit

---
+++ smart_home/measurement/views.py
@@ -0,0 +1,23 @@
+# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
+# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from rest_framework.viewsets import ModelViewSet
+
+from measurements.models import Measurement, Project
+from measurements.serializers import ProjectDetailSerializer, MeasurementSerializer
+
+
+class ProjectViewSet(ModelViewSet):
+    """ViewSet для проекта."""
+    # TODO: добавьте конфигурацию для объекта
+    queryset = Project.objects.all()
+    serializer_class = ProjectDetailSerializer
+
+
+
+class MeasurementViewSet(ModelViewSet):
+    """ViewSet для измерения."""
+    # TODO: добавьте конфигурацию для измерения
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
